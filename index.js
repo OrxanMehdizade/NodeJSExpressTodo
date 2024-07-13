64d7caaa5c9db9d3dd9720167258738f6ecd1cc9
Mehdizade Orxan Kerem <Mehdi_ha01@itstep.edu.az>
2024-07-13 11:54:18 +0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 require("dotenv").config();
-const express = require("express");
+const express = require('express');
 const mongoose = require("mongoose");
-const todoRoute = require("./routes/todoRoute");
-
-mongoose
-  .connect(process.env.CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error) => console.error("Error connecting to MongoDB:", error));
-
+const cors = require("cors");
+const todoRoutes = require("./routes/todoRoute");
+const authRoutes = require("./routes/authRoutes");
+const TODODB_URL = process.env.CONNECTION_STRING;
 
 const app = express();
-
 app.use(express.json());
+app.use(cors());
 
-app.use("/todos", todoRoute);
+mongoose
+    .connect(TODODB_URL)
+    .then(() => console.log("Connected to MongoDB Atlas"))
+    .catch((err) => console.error("Error connecting to MongoDB Atlas", err));
 
-const port = process.env.PORT || 5000;
+    
+app.use("/todos", todoRoutes);
+app.use("/auth", authRoutes);
 
+
+
+port = 5000;
 app.listen(port, () => {
-    console.log(`Server running on PID ${process.pid} at http://localhost:${port}`);
+    console.log(`Server running on port ${port}`);
 });
